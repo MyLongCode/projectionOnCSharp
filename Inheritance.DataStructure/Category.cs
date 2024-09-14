8c7bbd21eb420eb131c35b470223dfe443e9dd32
mylongcode <mylongcode@gmail.com>
2024-09-14 22:17:26 +0500

    add solution in practice

--- Inheritance.DataStructure/Category.cs
+++ Inheritance.DataStructure/Category.cs
@@ -1,6 +1,8 @@
-﻿namespace Inheritance.DataStructure;
+﻿using System.Collections;
 
-class Category : IComparable<Category>
+namespace Inheritance.DataStructure;
+
+class Category : IComparable
 {
     public string Name { get; set; }
     public MessageType MessageType { get; set; }
@@ -13,39 +15,69 @@ class Category : IComparable<Category>
         MessageTopic = messageTopic;
     }
 
-
-    public int CompareTo(Category other)
+    public static bool operator ==(Category c1, Category c2)
     {
+        return c1.CompareTo(c2) == 0;
+    }
+    public static bool operator !=(Category c1, Category c2)
+    {
+        return c1.CompareTo(c2) != 0;
+    }
+    public static bool operator <(Category c1, Category c2)
+    {
+        return c1.CompareTo(c2) < 0;
+    }
+    public static bool operator >(Category c1, Category c2)
+    {
+        return c1.CompareTo(c2) > 0;
+    }
+    public static bool operator <=(Category c1, Category c2)
+    {
+        return c1.CompareTo(c2) <= 0;
+    }
+    public static bool operator >=(Category c1, Category c2)
+    {
+        return c1.CompareTo(c2) >= 0;
+    }
+
+    public bool Equals(Category other)
+    {
+        if (other is null) return false;
+        if (this.Name == other.Name 
+            && this.MessageTopic == other.MessageTopic 
+            && this.MessageType == other.MessageType) return true;
+        return false;
+    }
+    public virtual int GetHashCode()
+    {
+        int total = 0;
+        foreach (var item in this.Name)
+            total += 7 * (int)item;
+        total += 11 * (int)this.MessageType;
+        total += 13 * (int)this.MessageTopic;
+        return total;
+    }
+    public string ToString()
+    {
+        return $"{this.Name}.{this.MessageType}.{this.MessageTopic}";
+    }
+    public int CompareTo(object? obj)
+    {
+        if (obj is null) return 1;
+        Category other = obj as Category;
+        if (this.Name is null || other.Name is null) return 1;
         if (this.Name == other.Name)
         {
             if (this.MessageType == other.MessageType)
             {
                 if (this.MessageTopic == other.MessageTopic)
                     return 0;
-                else if (this.MessageTopic > other.MessageTopic) return -1;
-                else return 1;
+                else if (this.MessageTopic > other.MessageTopic) return 1;
+                else return -1;
             }
-            else if (this.MessageType > other.MessageType) return -1;
-            else return 1;
+            else if (this.MessageType > other.MessageType) return 1;
+            else return -1;
         }
-        return this.Name.CompareTo(other.Name);    
-                
-    }
-
-    public bool Equals(Category other)
-    {
-        if (this.Name == other.Name 
-            && this.MessageTopic == other.MessageTopic 
-            && this.MessageType == other.MessageType) return true;
-        return false;
-    }
-    public override int GetHashCode()
-    {
-        int total = 0;
-        foreach (var item in this.Name)
-            total += 11 * (int)item;
-        total += 11 * this.MessageType.GetHashCode();
-        total += 11 * this.MessageTopic.GetHashCode();
-        return total;
+        return this.Name.CompareTo(other.Name);
     }
 }
