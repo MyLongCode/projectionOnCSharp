db2cd9a3c2656b4030100ed5e3e5bff377b42585
mylongcode <mylongcode@gmail.com>
2024-09-17 12:25:57 +0500

    add practice

---
+++ Incapsulation.Weights/Indexer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Weights
+{
+    public class Indexer
+    {
+        private double[] container;
+        private int begin;
+        private int end;
+
+        public Indexer(double[] array, int start, int length)
+        {
+            if (length > array.Length 
+                || start < 0 || length < 0 
+                || start + length > array.Length) throw new ArgumentException();
+            container = array;
+            this.begin = start;
+            this.end = length;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return end - begin + 1;
+            }
+        }
+
+        public double this[int num]
+        {
+            get
+            {
+                if (num < 0 || num >= Length) throw new IndexOutOfRangeException();
+                else return container[num + begin];
+            }
+            set
+            {
+                if (num < 0 || num >= Length) throw new IndexOutOfRangeException();
+                container[num + begin] = value;
+            }
+
+        }
+    }
+}
