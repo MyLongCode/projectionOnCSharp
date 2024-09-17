46ee87d48e32628a4383daaa3ed2c450edcdee27
mylongcode <mylongcode@gmail.com>
2024-09-17 12:24:09 +0500

    add practice

---
+++ Incapsulation.EnterpriseTask/Enterprise.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.EnterpriseTask
+{
+    public class Enterprise
+    {
+
+        public Guid Guid { get; }
+
+        public string Name { get; set; }
+
+
+        string inn;
+        public string Inn { 
+            get { return inn; } 
+            set 
+            {
+                if (value.Length != 10 || !value.All(z => char.IsDigit(z)))
+                    throw new ArgumentException();
+                this.inn = value;
+            } 
+        }
+
+        DateTime establishDate;
+
+        public DateTime EstablishDate { get; set; }
+
+        public TimeSpan ActiveTimeSpan { get { return DateTime.Now - establishDate; } }
+
+        public double GetTotalTransactionsAmount()
+        {
+            DataBase.OpenConnection();
+            var amount = 0.0;
+            foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == Guid))
+                amount += t.Amount;
+            return amount;
+        }
+    }
+}
