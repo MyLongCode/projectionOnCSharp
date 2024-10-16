c7325a4b568442f66b055792879b9fd93038585e
mylongcode <mylongcode@gmail.com>
2024-10-16 22:30:53 +0500

    add solution on practice

--- Delegates.Observers/ObservableStack.cs
+++ Delegates.Observers/ObservableStack.cs
@@ -4,76 +4,41 @@ namespace Delegates.Observers;
 
 public class StackOperationsLogger
 {
-	private readonly Observer observer = new();
-	public void SubscribeOn<T>(ObservableStack<T> stack)
-	{
-		stack.Add(observer);
-	}
+    public StringBuilder Logger = new StringBuilder();
 
-	public string GetLog()
-	{
-		return observer.Log.ToString();
-	}
+    public string GetLog() => Logger.ToString();
+
+    public void SubscribeOn<T>(ObservableStack<T> stack)
+    {
+        stack.OnStackChanged += (sender, e) =>
+        {
+            Logger.Append(e);
+        };
+    }
 }
 
-public interface IObserver
+public class ObservableStack<T>
 {
-	void HandleEvent(object eventData);
-}
+    public event EventHandler<StackEventData<T>> OnStackChanged;
+    List<T> stackData = new List<T>();
 
-public class Observer : IObserver
-{
-	public StringBuilder Log = new();
+    protected void Notify(StackEventData<T> eventData)
+    {
+        OnStackChanged?.Invoke(this, eventData);
+    }
 
-	public void HandleEvent(object eventData)
-	{
-		Log.Append(eventData);
-	}
-}
+    public T Pop()
+    {
+        if (stackData.Count == 0)
+            throw new InvalidOperationException();
+        var last_element = stackData[stackData.Count - 1];
+        Notify(new StackEventData<T> { IsPushed = false, Value = last_element });
+        return last_element;
+    }
 
-public interface IObservable
-{
-	void Add(IObserver observer);
-	void Remove(IObserver observer);
-	void Notify(object eventData);
-}
-
-
-public class ObservableStack<T> : IObservable
-{
-	List<IObserver> observers = new();
-
-	public void Add(IObserver observer)
-	{
-		observers.Add(observer);
-	}
-
-	public void Notify(object eventData)
-	{
-		foreach (var observer in observers)
-			observer.HandleEvent(eventData);
-	}
-
-	public void Remove(IObserver observer)
-	{
-		observers.Remove(observer);
-	}
-
-	List<T> data = new();
-
-	public void Push(T obj)
-	{
-		data.Add(obj);
-		Notify(new StackEventData<T> { IsPushed = true, Value = obj });
-	}
-
-	public T Pop()
-	{
-		if (data.Count == 0)
-			throw new InvalidOperationException();
-		var result = data[data.Count - 1];
-		Notify(new StackEventData<T> { IsPushed = false, Value = result });
-		return result;
-
-	}
+    public void Push(T element)
+    {
+        stackData.Add(element);
+        Notify(new StackEventData<T> { IsPushed = true, Value = element });
+    }
 }
