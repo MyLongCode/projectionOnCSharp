18c2e9f57c0b1fdc996bff9a5fd695c7586f4ef4
mylongcode <mylongcode@gmail.com>
2024-10-18 21:49:23 +0500

    add practice solution

--- Delegates.TreeTraversal/Traversal.cs
+++ Delegates.TreeTraversal/Traversal.cs
@@ -1,19 +1,76 @@
+using System;
+using System.IO.Pipes;
+
 namespace Delegates.TreeTraversal;
 
 public static class Traversal
 {
+
 	public static IEnumerable<Product> GetProducts(ProductCategory root)
 	{
-		return root.Products.ToList();
+        var travesal = new Traversal<ProductCategory, Product>(
+            (productCategory) => productCategory.Products,
+            (productsCategory) => productsCategory.Categories,
+            (productsCategory) => true
+            );
+        var ans = new List<Product>();
+        travesal.PostOrder(root, ans);
+        return ans;
 	}
 
-	public static IEnumerable<Job> GetEndJobs(Job root)
-	{
-		throw new NotImplementedException();
-	}
+    public static IEnumerable<Job> GetEndJobs(Job root)
+    {
+        var traversal = new Traversal<Job, Job>(
+                job => new List<Job> { job },
+                job => job.Subjobs,
+                job => job.Subjobs == null || job.Subjobs.Count == 0
+            );
+        var ans = new List<Job>();
+        traversal.PostOrder(root, ans);
+        return ans;
+    }
 
 	public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
 	{
-		throw new NotImplementedException();
-	}
+        var traversal = new Traversal<BinaryTree<T>, T>(
+                tree =>
+                new List<T> { tree.Value },
+                tree =>
+                {
+                    var nodes = new List<BinaryTree<T>>();
+                    if (tree.Left != null) nodes.Add(tree.Left);
+                    if (tree.Right != null) nodes.Add(tree.Right);
+
+                    return nodes;
+                },
+                tree => tree.Right == null && tree.Left == null
+                );
+        var ans = new List<T>();
+        traversal.PostOrder(root, ans);
+        return ans;
+    }
+}
+public class Traversal<TTree, Titem>
+{
+    Func<TTree, IEnumerable<Titem>> treeNodeHandler;
+    Func<TTree, IEnumerable<TTree>> treeParser;
+    Func<TTree, bool> isValid;
+
+    public Traversal(Func<TTree, IEnumerable<Titem>> treeNodeHandler,
+                Func<TTree, IEnumerable<TTree>> treeParser,
+                Func<TTree, bool> isValid)
+    {
+        this.treeNodeHandler = treeNodeHandler;
+        this.treeParser = treeParser;
+        this.isValid = isValid;
+    }
+    public void PostOrder(TTree tree, List<Titem> result)
+    {
+        if (isValid(tree))
+            result.AddRange(treeNodeHandler(tree));
+
+        var next = treeParser(tree);
+        foreach (var node in next)
+            PostOrder(node, result);
+    }
 }
